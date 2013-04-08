789cee024342b3baf08327a745c64a81fc85509a
ParkYongTak <pwiner85@gmail.com>
2013-04-09 08:40:08 +0900

    ProjectA

--- ProjectA/src/kr/example/profecta/ProjectA.java
+++ ProjectA/src/kr/example/profecta/ProjectA.java
@@ -1,5 +1,9 @@
 package kr.example.profecta;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -19,6 +23,14 @@ public class ProjectA extends Activity {
 
 		tv = (TextView) findViewById(R.id.test_text_view1);
 
+//		String temp;
+//		File fileName = new File("c:/android/workspace4/ProjectA/dbfile.txt");
+//		BufferedReader br = new BufferedReader(new FileReader(fileName));
+//		while ((temp = br.readLine()) != null) {
+//			System.out.println(temp);
+//		}
+//		br.close();
+
 	}
 
 	@Override
@@ -33,7 +45,6 @@ public class ProjectA extends Activity {
 		case R.id.button1:
 
 			int index1 = gbb.result();
-
 
 			tv.setText("가위를 내셨습니다.");
 			if (index1 == 0) {
@@ -53,7 +64,7 @@ public class ProjectA extends Activity {
 		case R.id.button2:
 
 			int index2 = gbb.result();
-			
+
 			tv.setText("바위를 내셨습니다.");
 			if (index2 == 0) {
 				Toast.makeText(ProjectA.this, "바위 내셨습니다. 이겼습니다.",
@@ -72,7 +83,7 @@ public class ProjectA extends Activity {
 		case R.id.button3:
 
 			int index3 = gbb.result();
-			
+
 			tv.setText("보를 내셨습니다.");
 			if (index3 == 0) {
 				Toast.makeText(ProjectA.this, "보자기를 내셨습니다. 이겼습니다.",
