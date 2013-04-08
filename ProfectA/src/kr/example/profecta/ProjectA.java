fb4032221a6e9df9751b15362c1d1979e3ec3594
ParkYongTak <pwiner85@gmail.com>
2013-04-08 18:16:27 +0900

    ProjectA

---
+++ ProfectA/src/kr/example/profecta/ProjectA.java
@@ -0,0 +1,71 @@
+package kr.example.profecta;
+
+import java.util.Random;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class ProjectA extends Activity {
+	
+	Random r1 = new Random();
+	
+	int index = r1.nextInt(3);
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.project, menu);
+		return true;
+	}
+	
+	public void myClickHandler(View v) {
+		switch(v.getId()){
+		case R.id.button1:
+			if(index == 0){
+				Toast.makeText(ProjectA.this, "가위를 내셨습니다. 이겼습니다.", Toast.LENGTH_SHORT).show();				
+				return;
+			} else if(index == 1) {
+				Toast.makeText(ProjectA.this, "가위를 내셨습니다. 졌습니다.", Toast.LENGTH_SHORT).show();
+				return;
+			} else {
+				Toast.makeText(ProjectA.this, "가위를 내셨습니다. 비겼습니다.", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			//break;
+		case R.id.button2:
+			if(index == 0){
+				Toast.makeText(ProjectA.this, "바위 내셨습니다. 이겼습니다.", Toast.LENGTH_SHORT).show();				
+				return;
+			} else if(index == 1) {
+				Toast.makeText(ProjectA.this, "바위 내셨습니다. 졌습니다.", Toast.LENGTH_SHORT).show();
+				return;
+			} else {
+				Toast.makeText(ProjectA.this, "바위 내셨습니다. 비겼습니다.", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			//break;
+		case R.id.button3:
+			if(index == 0){
+				Toast.makeText(ProjectA.this, "보자기를 내셨습니다. 이겼습니다.", Toast.LENGTH_SHORT).show();				
+				return;
+			} else if(index == 1) {
+				Toast.makeText(ProjectA.this, "보자기를 내셨습니다. 졌습니다.", Toast.LENGTH_SHORT).show();
+				return;
+			} else {
+				Toast.makeText(ProjectA.this, "보자기를 내셨습니다. 비겼습니다.", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			//break;
+		}
+    }	
+
+}
