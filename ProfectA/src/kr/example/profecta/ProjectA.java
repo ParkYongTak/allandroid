7f5093927deef581383e85dd627858e23f99daa8
ParkYongTak <pwiner85@gmail.com>
2013-04-09 02:06:13 +0900

    ProjectA

--- ProfectA/src/kr/example/profecta/ProjectA.java
+++ ProfectA/src/kr/example/profecta/ProjectA.java
@@ -1,7 +1,5 @@
 package kr.example.profecta;
 
-import java.util.Random;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,16 +8,17 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class ProjectA extends Activity {
-	
+
+	GBB gbb = new GBB();
 	TextView tv;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+
 		tv = (TextView) findViewById(R.id.test_text_view1);
-		
+
 	}
 
 	@Override
@@ -28,61 +27,68 @@ public class ProjectA extends Activity {
 		getMenuInflater().inflate(R.menu.project, menu);
 		return true;
 	}
-	
+
 	public void myClickHandler(View v) {
-		switch(v.getId()){
+		switch (v.getId()) {
 		case R.id.button1:
-			
-			Random r1 = new Random();
-			int index1 = r1.nextInt(3);
-			
+
+			int index1 = gbb.result();
+
+
 			tv.setText("가위를 내셨습니다.");
-			if(index1 == 0){
-				Toast.makeText(ProjectA.this, "가위를 내셨습니다. 이겼습니다.", Toast.LENGTH_SHORT).show();
+			if (index1 == 0) {
+				Toast.makeText(ProjectA.this, "가위를 내셨습니다. 이겼습니다.",
+						Toast.LENGTH_SHORT).show();
 				return;
-			} else if(index1 == 1) {
-				Toast.makeText(ProjectA.this, "가위를 내셨습니다. 졌습니다.", Toast.LENGTH_SHORT).show();
+			} else if (index1 == 1) {
+				Toast.makeText(ProjectA.this, "가위를 내셨습니다. 졌습니다.",
+						Toast.LENGTH_SHORT).show();
 				return;
 			} else {
-				Toast.makeText(ProjectA.this, "가위를 내셨습니다. 비겼습니다.", Toast.LENGTH_SHORT).show();
+				Toast.makeText(ProjectA.this, "가위를 내셨습니다. 비겼습니다.",
+						Toast.LENGTH_SHORT).show();
 				return;
 			}
-			//break;
+			// break;
 		case R.id.button2:
-			
-			Random r2 = new Random();
-			int index2 = r2.nextInt(3);
+
+			int index2 = gbb.result();
 			
 			tv.setText("바위를 내셨습니다.");
-			if(index2 == 0){
-				Toast.makeText(ProjectA.this, "바위 내셨습니다. 이겼습니다.", Toast.LENGTH_SHORT).show();				
+			if (index2 == 0) {
+				Toast.makeText(ProjectA.this, "바위 내셨습니다. 이겼습니다.",
+						Toast.LENGTH_SHORT).show();
 				return;
-			} else if(index2 == 1) {
-				Toast.makeText(ProjectA.this, "바위 내셨습니다. 졌습니다.", Toast.LENGTH_SHORT).show();
+			} else if (index2 == 1) {
+				Toast.makeText(ProjectA.this, "바위 내셨습니다. 졌습니다.",
+						Toast.LENGTH_SHORT).show();
 				return;
 			} else {
-				Toast.makeText(ProjectA.this, "바위 내셨습니다. 비겼습니다.", Toast.LENGTH_SHORT).show();
+				Toast.makeText(ProjectA.this, "바위 내셨습니다. 비겼습니다.",
+						Toast.LENGTH_SHORT).show();
 				return;
 			}
-			//break;
+			// break;
 		case R.id.button3:
-			
-			Random r3 = new Random();
-			int index3 = r3.nextInt(3);
+
+			int index3 = gbb.result();
 			
 			tv.setText("보를 내셨습니다.");
-			if(index3 == 0){
-				Toast.makeText(ProjectA.this, "보자기를 내셨습니다. 이겼습니다.", Toast.LENGTH_SHORT).show();				
+			if (index3 == 0) {
+				Toast.makeText(ProjectA.this, "보자기를 내셨습니다. 이겼습니다.",
+						Toast.LENGTH_SHORT).show();
 				return;
-			} else if(index3 == 1) {
-				Toast.makeText(ProjectA.this, "보자기를 내셨습니다. 졌습니다.", Toast.LENGTH_SHORT).show();
+			} else if (index3 == 1) {
+				Toast.makeText(ProjectA.this, "보자기를 내셨습니다. 졌습니다.",
+						Toast.LENGTH_SHORT).show();
 				return;
 			} else {
-				Toast.makeText(ProjectA.this, "보자기를 내셨습니다. 비겼습니다.", Toast.LENGTH_SHORT).show();
+				Toast.makeText(ProjectA.this, "보자기를 내셨습니다. 비겼습니다.",
+						Toast.LENGTH_SHORT).show();
 				return;
 			}
-			//break;
+			// break;
 		}
-    }	
+	}
 
 }
